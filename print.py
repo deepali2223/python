ab2d2acb76ebef61307d1c8f0fc5afe6d1f7f0d8
deepali2223 <120616967+deepali2223@users.noreply.github.com>
2023-02-14 21:22:38 +0530

    Update print.py

--- print.py
+++ print.py
@@ -1,6 +1,6 @@
 #print( 'hey ' + input("enter your name") + ',\nhow are you?')
 name = "deee"
-print("fjdh")
-print("dakf")
 a=len(name)
 print(a)
+print(r'DEEPALI BISHT')
+print("deepali \' s car")
