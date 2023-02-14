9f337ae5188a2dcacc790a91aa97b308cc66c96b
deepali2223 <120616967+deepali2223@users.noreply.github.com>
2023-02-14 22:04:01 +0530

    Add files via upload

---
+++ swapping without 3rd variable.py
@@ -0,0 +1,7 @@
+a = int(input("enter a number "))
+b= int(input("enter a numebr"))
+print("the value of a is {0} and b is {1} before swapping".format(a,b))
+a = a+b
+b= a-b
+a= a-b
+print("the value of a is {0} and b is {1} after swapping".format(a,b))
