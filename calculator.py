03a877bf54874572ebfd7d27cfdaa90c3129d687
deepali2223 <120616967+deepali2223@users.noreply.github.com>
2023-02-14 21:16:51 +0530

    Update and rename first.py to calculator.py

---
+++ calculator.py
@@ -0,0 +1,9 @@
+
+x = int(input("enter a number"))
+y= int(input("enter a number"))
+print("the sum of both number is",x+y)
+print("the difference of both number is",x-y)
+print("the multiplication of both number is",x*y)
+print("the division of both number is",x/y)
+print("the remainder of both number is"x%y)
+
--- first.py
+++
@@ -1,4 +0,0 @@
-
-x = int(input("enter a number"))
-y= int(input("enter a number"))
-print("the sum of both number is",x+y)
